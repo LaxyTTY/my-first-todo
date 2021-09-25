097ade326d1f0df71990aaf0b16cf12f9abe7993
LaxyTTY <m.goncearuc344@gmail.com>
2021-09-25 18:06:10 +0300

    Add and delete todo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-import "./styles.css";
+let toDoContainer = document.querySelector('.container_list')
+let inputField = document.querySelector('.task_input')
+let addToDoBtn = document.querySelector('.save_btn')
+let allTasks = document.querySelectorAll('.todo_element')
 
-document.getElementById("app").innerHTML = `
+addToDoBtn.onclick = function () {
+    const div = document.createElement('div');
+    div.className = "todo_element";
+    div.innerHTML = `
+      <p class="todo_element_title">${inputField.value}</p>
+      <div class="todo_element_buttons" >
+        <button>+</button>
+        <button class="delete_task">-</button>
+      </div>
+    `;
+    inputField.value = ''
+    toDoContainer.appendChild(div)
+}
 
-`;
+toDoContainer.addEventListener('click', function (e) {
+    const item = e.target
+    if(item.classList[0] === "delete_task") {
+        item.parentElement.parentElement.remove()
+    }
+})
+
+
+
+
+
+
